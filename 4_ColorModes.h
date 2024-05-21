8803d222057bbe86647b4f8985656c9cb4be15b0
bvandercar-vt <blake.vandercar@validatek.com>
2024-05-20 20:10:58 -0600

    improve chase numbers

--- 4_ColorModes.h
+++ 4_ColorModes.h
@@ -8,8 +8,10 @@ enum ChaseDirection : uint8_t {
 
 extern void fillRainbowEdges(uint16_t first_pixel_hue, uint8_t num_cycles = 1);
 extern void fillRainbowMiddles(uint16_t first_pixel_hue, uint8_t num_cycles = 2);
-extern void rainbowChaseEdges(uint8_t num_cycles, uint16_t rage_hue_step);
-extern void rainbowChaseQuadrants(uint8_t num_cycles, uint16_t rage_hue_step);
+extern void rainbowChaseEdges(uint8_t num_cycles, uint16_t min_wait_millis, uint16_t max_wait_millis,
+                              uint16_t rage_hue_step);
+extern void rainbowChaseQuadrants(uint8_t num_cycles, uint16_t min_wait_millis, uint16_t max_wait_millis,
+                                  uint16_t rage_hue_step);
 extern void rainbowFade();
 extern void theaterChaseCycle();
 extern void tapFlash();
